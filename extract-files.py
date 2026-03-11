2982a2982f839cdb611b97c86a14bf5f6698ef16
hzj5158888 <www1693303798@163.com>
2026-03-11 13:07:39 +0800

    zeus => mayfly

--- extract-files.py
+++ extract-files.py
@@ -18,7 +18,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/xiaomi/miuicamera-zeus',
+    'device/xiaomi/miuicamera-mayfly',
 ]
 
 
@@ -44,7 +44,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'miuicamera-zeus',
+    'miuicamera-mayfly',
     'xiaomi',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
